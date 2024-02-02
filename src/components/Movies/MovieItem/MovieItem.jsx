146aeaa5722bf271d33ad4efec5b4bad3db30d6d
Dedek Tegar Apriyandi <dedektegarr@gmail.com>
2024-02-02 20:22:50 +0700

    adding lazy loading for movie list image

--- src/components/Movies/MovieItem/MovieItem.jsx
+++ src/components/Movies/MovieItem/MovieItem.jsx
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom";
 import { formatDate } from "../../../utils/format-date";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const MovieItem = ({ movie }) => {
   return (
     <Link to="">
       <article className="w-[150px] lg:w-[250px]">
-        <img
+        <LazyLoadImage
           src={`https://media.themoviedb.org/t/p/w300_and_h450_bestv2${movie.poster_path}`}
           alt="Movie poster"
           className="w-full h-[210px] lg:h-[350px] object-cover rounded-md shadow-lg"
+          effect="blur"
         />
         <div className="p-1 mt-2">
           <h3 className="font-bold">{movie.title || movie.name}</h3>
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./globals.css";
 import { Provider } from "react-redux";
 import store from "./store";
+import App from "./App";
+import "./globals.css";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
