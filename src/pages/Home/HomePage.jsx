9ade5859dfb079fe69fcfcadaa79d54689216fe9
Dedek Tegar Apriyandi <dedektegarr@gmail.com>
2024-02-02 13:15:29 +0700

    fix bug

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,21 +1,13 @@
 import { defer, useLoaderData } from "react-router-dom";
 import { fetchTrendingMovies } from "../../api/trending-movies";
 import MovieList from "../../components/Movies/MovieList/MovieList";
-import { useState } from "react";
 
 const HomePage = () => {
-  const [filter, setFilter] = useState({
-    trending: "day",
-  });
   const { trendingMovies } = useLoaderData();
 
   return (
     <>
-      <MovieList
-        movies={trendingMovies.results}
-        title="Trending"
-        filter={filter.trending}
-      />
+      <MovieList movies={trendingMovies.results} title="Trending" />
     </>
   );
 };
